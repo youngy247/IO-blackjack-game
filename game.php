acda1c3fd374b2175f8f87c61e74816f9a0bca37
Adam Young <adamyoungy678@gmail.com>
2023-03-22 18:19:24 +0000

    used for each loops to create the deck

--- game.php
+++ game.php
@@ -1,83 +1,33 @@
 <?php
 
-
-
-
-function deal() {
-
-    $deck = [
-        '2-h',
-        '3-h',
-        '4-h',
-        '5-h',
-        '6-h',
-        '7-h',
-        '8-h',
-        '9-h',
-        '10-h',
-        'J-h',
-        'Q-h',
-        'K-h',
-        'A-h',
-        '2-c',
-        '3-c',
-        '4-c',
-        '5-c',
-        '6-c',
-        '7-c',
-        '8-c',
-        '9-c',
-        '10-c',
-        'J-c',
-        'Q-c',
-        'K-c',
-        'A-c',
-        '2-s',
-        '3-s',
-        '4-s',
-        '5-c',
-        '6-c',
-        '7-c',
-        '8-c',
-        '9-c',
-        '10-c',
-        'J-c',
-        'Q-c',
-        'K-c',
-        'A-c',
-        '2-d',
-        '3-d',
-        '4-d',
-        '5-d',
-        '6-d',
-        '7-d',
-        '8-d',
-        '9-d',
-        '10-d',
-        'J-d',
-        'Q-d',
-        'K-d',
-        'A-d',
+//Define the cards and suits
+$suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+$cards = [
+    '2'=>2,
+    '3'=>3,
+    '4'=>4,
+    '5'=>5,
+    '6'=>6,
+    '7'=>7,
+    '8'=>8,
+    '9'=>9,
+    '10'=>10,
+    'J'=>10,
+    'Q'=>10,
+    'K'=>10,
+    'A'=>11
     ];
 
-
-    shuffle($suits);
-    shuffle($numbers);
-    $dealCard1 = $suits[array_rand($suits)] . '-' . array_rand($numbers) . '<p></p>';
-    $dealCard2 = $suits[array_rand($suits)] . '-' . array_rand($numbers);
-    $player1Cards = $dealCard1 . $dealCard2;
-//    $player2Cards = $dealCard1 . $dealCard2;
-    $dealtCards=[];
-    $array_push($)
-    echo $player1Cards;
-//    echo $player2Cards;
-
-
-};
-echo deal();
-
-function playGame(){
-    deal();
-
+//Create the deck
+$deck = array();
+foreach ($suits as $suit) {
+    foreach ($cards as $card) {
+        $deck[] = $card . " of " . $suit;
+    }
 }
+echo '<pre>';
+print_r($deck);
+echo '</pre';
+
+
 
